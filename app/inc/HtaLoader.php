d076c69a4038f75e3e04ae86eedb6a100a494a77
Gavin Gilmour <gavin.gilmour@gmail.com>
2014-03-01 18:05:35 +0000

    quotes

--- app/inc/HtaLoader.php
+++ app/inc/HtaLoader.php
@@ -40,9 +40,9 @@ class HTALoader
     public function fetch(array $term=null)
     {
         if (is_null($term)) {
-            $b = R::find("hta", '1 ORDER BY name DESC');
+            $b = R::find('hta', '1 ORDER BY name DESC');
         } else {
-            $b = R::find('hta',"name LIKE :name ORDER BY name DESC", array(
+            $b = R::find('hta','name LIKE :name ORDER BY name DESC', array(
                 ':name' => '%'.$term['name'].'%'
             ));
         }
