54cd529abd6e53b7d52b3572c4dc90711a8f1fdf
Gavin Gilmour <gavin.gilmour@gmail.com>
2014-03-02 09:56:15 +0000

    added example database

---
+++ app/data/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE hta (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    url TEXT,
+    graphic TEXT
+);
+
+INSERT INTO "hta" VALUES(NULL,
+  'BBC News',
+  'http://news.bbc.co.uk/',
+  'yellow'
+);
